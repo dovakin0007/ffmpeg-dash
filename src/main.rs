71d3fffe48c0050db713afa49e0ad8a2a574e97c
Gowtham Krishnamoorthy <dovakin0007@gmail.com>
2026-06-28 19:42:44 +0530

    add: rescale video

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,45 @@
 use std::{
-    error::Error,
     ffi::{CString, c_int},
+    fs,
     ops::{Deref, DerefMut},
-    path::Path,
+    path::{Path, PathBuf},
     ptr::null_mut,
+    str::FromStr,
 };
 
 use ffmpeg_next::{
-    Codec, Format, Frame, Packet, Rounding, Stream,
-    codec::{Context, Parameters},
+    Codec, Format, Frame, Packet, Rounding,
+    codec::{Context, Parameters, codec},
     decoder::{self, Opened},
-    device::input,
-    ffi::{avformat_close_input, avformat_free_context, avio_close, avio_closep},
-    format::{
-        self, Context as FormatContext, Flags, Input, Output,
-        context::{Destructor, output::dump},
-        open,
-    },
+    encoder::encoder,
+    ffi::avcodec_parameters_from_context,
+    format::{self, Context as FormatContext, Flags, Input, Pixel, context::output::dump, open},
+    media::Type::Video,
     packet,
 };
+
 use ffmpeg_next::{
     ffi::{AVIO_FLAG_WRITE, avcodec_parameters_copy, avformat_new_stream, avio_open},
     util::mathematics::rescale::Rescale,
 };
+
+pub fn rename_mpd_tmp() -> std::io::Result<()> {
+    for entry in fs::read_dir(".")? {
+        let path = entry?.path();
+        let Some(name) = path.file_name().and_then(|s| s.to_str()) else {
+            continue;
+        };
+        if name.ends_with("mpd.tmp") {
+            let final_path = Path::new(name.trim_end_matches(".tmp"));
+            if final_path.exists() {
+                std::fs::remove_file(final_path)?;
+            }
+            fs::rename(&path, final_path)?;
+            println!("Renamed {:?} -> {:?}", path, final_path);
+        }
+    }
+    Ok(())
+}
 
 pub struct CodecCtx {
     open: Opened,
@@ -143,6 +160,7 @@ impl DerefMut for WrappedFrame {
 fn convert_input_streams(
     input: &mut format::context::Input,
     output: &mut format::context::Output,
+    encoder: &ffmpeg_next::encoder::Encoder,
 ) -> Vec<i32> {
     let mut stream_list = vec![-1; input.nb_streams() as usize];
     let mut stream_index = 0;
@@ -151,18 +169,26 @@ fn convert_input_streams(
 
     for stream in input.streams() {
         let params = stream.parameters();
-
         match params.medium() {
             ffmpeg_next::media::Type::Video
             | ffmpeg_next::media::Type::Audio
             | ffmpeg_next::media::Type::Subtitle => unsafe {
                 let out_stream = avformat_new_stream(out_ctx, std::ptr::null());
-
                 if out_stream.is_null() {
                     panic!("avformat_new_stream failed");
                 }
 
-                let ret = avcodec_parameters_copy((*out_stream).codecpar, params.as_ptr());
+                let ret = match params.medium() {
+                    ffmpeg_next::media::Type::Video => {
+                        avcodec_parameters_from_context((*out_stream).codecpar, encoder.as_ptr())
+                    }
+
+                    ffmpeg_next::media::Type::Audio | ffmpeg_next::media::Type::Subtitle => {
+                        avcodec_parameters_copy((*out_stream).codecpar, params.as_ptr())
+                    }
+
+                    _ => unreachable!(),
+                };
 
                 if ret < 0 {
                     panic!("avcodec_parameters_copy failed: {}", ret);
@@ -192,7 +218,6 @@ fn convert_input_streams(
             _ => {}
         }
     }
-
     dump(output, 0, None);
 
     stream_list
@@ -206,6 +231,7 @@ fn create_new_file<P: AsRef<Path>>(
     input: &mut format::context::Input,
     output_ctx: &mut format::context::Output,
     path: P,
+    encoder: &ffmpeg_next::encoder::Encoder,
 ) -> Vec<i32> {
     if (output_ctx.format().flags() & Flags::NO_FILE).bits() == 0 {
         let filename = CString::new(path.as_ref().to_str().unwrap()).unwrap();
@@ -220,7 +246,7 @@ fn create_new_file<P: AsRef<Path>>(
             }
         }
     }
-    let stream_index = convert_input_streams(input, output_ctx);
+    let stream_index = convert_input_streams(input, output_ctx, encoder);
 
     for (i, stream) in input.streams().enumerate() {
         println!("Input stream {} TB = {:?}", i, stream.time_base());
@@ -260,11 +286,59 @@ fn create_new_file<P: AsRef<Path>>(
 fn main() {
     let mut packet = Packet::empty();
     let demuxer = DemuxerCtx::new("./small_bunny_1080p_60fps.mp4").unwrap();
-
     let input_ctx: &mut format::context::Input = &mut demuxer.ctx.input();
+    let codec_ctx = ffmpeg_next::codec::context::Context::from_parameters(
+        input_ctx
+            .streams()
+            .best(ffmpeg_next::media::Type::Video)
+            .unwrap()
+            .parameters(),
+    )
+    .unwrap();
+    let mut decoder = codec_ctx.decoder().video().unwrap();
+    println!("decoder id = {:?}", decoder.id());
+    let mut scaler = ffmpeg_next::software::scaling::Context::get(
+        decoder.format(),
+        decoder.width(),
+        decoder.height(),
+        Pixel::YUV420P,
+        1280,
+        720,
+        ffmpeg_next::software::scaling::Flags::BILINEAR,
+    )
+    .unwrap();
+
+    let encoder_codec = ffmpeg_next::codec::encoder::find_by_name("h264_mf").unwrap();
+    println!("encoder name = {}", encoder_codec.name());
+    println!("encoder id = {:?}", encoder_codec.id());
+    let mut encoder = ffmpeg_next::codec::context::Context::new_with_codec(encoder_codec)
+        .encoder()
+        .video()
+        .unwrap();
+
+    encoder.set_width(1280);
+    encoder.set_height(720);
+    encoder.set_format(Pixel::YUV420P);
+    let video_stream = input_ctx
+        .streams()
+        .best(ffmpeg_next::media::Type::Video)
+        .unwrap();
+
+    encoder.set_time_base(video_stream.time_base());
+    encoder.set_frame_rate(Some(video_stream.rate()));
+    println!("decoder tb = {:?}", decoder.time_base());
+    println!("decoder fr = {:?}", decoder.frame_rate());
     let mut output = ffmpeg_next::format::output("./its_mpd.mpd").unwrap();
+    if output
+        .format()
+        .flags()
+        .contains(format::Flags::GLOBAL_HEADER)
+    {
+        encoder.set_flags(ffmpeg_next::codec::Flags::GLOBAL_HEADER);
+    }
+    let mut encoder = encoder.open().unwrap();
     let output_ctx = &mut output;
-    let stream_index_list = create_new_file(input_ctx, output_ctx, "./its_mpd.mpd");
+    let stream_index_list = create_new_file(input_ctx, output_ctx, "./its_mpd.mpd", &encoder);
     let streams_count = input_ctx.nb_streams();
     'stream: loop {
         match packet.read(input_ctx) {
@@ -281,46 +355,180 @@ fn main() {
         let in_index = packet.stream();
 
         let instream = input_ctx.stream(in_index).unwrap();
-        if packet.stream() >= streams_count as usize || stream_index_list[packet.stream()] < 0 {
-            continue 'stream;
+        match instream.parameters().medium() {
+            ffmpeg_next::media::Type::Video => {
+                decoder.send_packet(&packet).unwrap();
+                let mut decoded = ffmpeg_next::util::frame::video::Video::empty();
+                let mut encoded_packet = ffmpeg_next::packet::Packet::empty();
+                while decoder.receive_frame(&mut decoded).is_ok() {
+                    let mut new_frame = ffmpeg_next::util::frame::video::Video::empty();
+                    scaler.run(&decoded, &mut new_frame).unwrap();
+                    println!(
+                        "decoded: pts={:?} width={} height={} format={:?}",
+                        decoded.pts(),
+                        decoded.width(),
+                        decoded.height(),
+                        decoded.format(),
+                    );
+
+                    println!(
+                        "scaled: pts={:?} width={} height={} format={:?}",
+                        new_frame.pts(),
+                        new_frame.width(),
+                        new_frame.height(),
+                        new_frame.format(),
+                    );
+                    new_frame.set_pts(decoded.pts());
+                    encoder.send_frame(&new_frame).unwrap();
+
+                    while encoder.receive_packet(&mut encoded_packet).is_ok() {
+                        let out_index = stream_index_list[in_index] as usize;
+                        encoded_packet.set_stream(out_index);
+                        let out_index = stream_index_list[in_index] as usize;
+
+                        let out_stream = output_ctx.stream(encoded_packet.stream()).unwrap();
+                        if let Some(dts) = encoded_packet.dts() {
+                            encoded_packet.set_dts(Some(dts.rescale_with(
+                                encoder.time_base(),
+                                out_stream.time_base(),
+                                Rounding::PassMinMax,
+                            )));
+                        }
+                        if let Some(pts) = encoded_packet.pts() {
+                            encoded_packet.set_pts(Some(pts.rescale_with(
+                                encoder.time_base(),
+                                out_stream.time_base(),
+                                Rounding::PassMinMax,
+                            )));
+                        }
+
+                        encoded_packet.set_duration(
+                            encoded_packet
+                                .duration()
+                                .rescale(encoder.time_base(), out_stream.time_base()),
+                        );
+
+                        encoded_packet.set_position(-1);
+
+                        match encoded_packet.write_interleaved(output_ctx) {
+                            Ok(_) => {}
+                            Err(e) => {
+                                println!("write_interleaved error: {:?}", e);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            ffmpeg_next::media::Type::Audio | ffmpeg_next::media::Type::Subtitle => {
+                if packet.stream() >= streams_count as usize
+                    || stream_index_list[packet.stream()] < 0
+                {
+                    continue 'stream;
+                }
+
+                let out_index = stream_index_list[in_index] as usize;
+
+                packet.set_stream(out_index);
+                let out_stream = output_ctx.stream(packet.stream()).unwrap();
+                if let Some(dts) = packet.dts() {
+                    packet.set_dts(Some(dts.rescale_with(
+                        instream.time_base(),
+                        out_stream.time_base(),
+                        Rounding::PassMinMax,
+                    )));
+                }
+                if let Some(pts) = packet.pts() {
+                    packet.set_pts(Some(pts.rescale_with(
+                        instream.time_base(),
+                        out_stream.time_base(),
+                        Rounding::PassMinMax,
+                    )));
+                }
+
+                packet.set_duration(
+                    packet
+                        .duration()
+                        .rescale(instream.time_base(), out_stream.time_base()),
+                );
+
+                packet.set_position(-1);
+
+                match packet.write_interleaved(output_ctx) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("write_interleaved error: {:?}", e);
+                        break;
+                    }
+                }
+            }
+            _ => {}
         }
+    }
 
-        let out_index = stream_index_list[in_index] as usize;
+    let video_in_index = input_ctx
+        .streams()
+        .best(ffmpeg_next::media::Type::Video)
+        .unwrap()
+        .index();
 
-        let out_stream = output_ctx.stream(out_index).unwrap();
+    let video_out_index = stream_index_list[video_in_index] as usize;
 
-        packet.set_stream(out_index);
-        let out_stream = output_ctx.stream(packet.stream()).unwrap();
-        if let Some(dts) = packet.dts() {
-            packet.set_dts(Some(dts.rescale_with(
-                instream.time_base(),
-                out_stream.time_base(),
-                Rounding::PassMinMax,
-            )));
-        }
-        if let Some(pts) = packet.pts() {
-            packet.set_pts(Some(pts.rescale_with(
-                instream.time_base(),
-                out_stream.time_base(),
-                Rounding::PassMinMax,
-            )));
-        }
+    let mut decoded = ffmpeg_next::frame::Video::empty();
+    let mut scaled = ffmpeg_next::frame::Video::empty();
+    let mut encoded = ffmpeg_next::Packet::empty();
 
-        packet.set_duration(
-            packet
-                .duration()
-                .rescale(instream.time_base(), out_stream.time_base()),
+    // Flush decoder
+    decoder.send_eof().unwrap();
+
+    while decoder.receive_frame(&mut decoded).is_ok() {
+        scaler.run(&decoded, &mut scaled).unwrap();
+
+        println!(
+            "decoded: pts={:?} width={} height={} format={:?}",
+            decoded.pts(),
+            decoded.width(),
+            decoded.height(),
+            decoded.format(),
         );
 
-        packet.set_position(-1);
+        println!(
+            "scaled: pts={:?} width={} height={} format={:?}",
+            scaled.pts(),
+            scaled.width(),
+            scaled.height(),
+            scaled.format(),
+        );
+        scaled.set_pts(decoded.pts());
 
-        match packet.write_interleaved(output_ctx) {
-            Ok(_) => {}
-            Err(e) => {
-                println!("write_interleaved error: {:?}", e);
-                break;
-            }
+        encoder.send_frame(&scaled).unwrap();
+
+        while encoder.receive_packet(&mut encoded).is_ok() {
+            encoded.set_stream(video_out_index);
+
+            let out_stream = output_ctx.stream(video_out_index).unwrap();
+
+            encoded.rescale_ts(encoder.time_base(), out_stream.time_base());
+
+            encoded.set_position(-1);
+
+            encoded.write_interleaved(output_ctx).unwrap();
         }
+    }
+
+    // Flush encoder
+    encoder.send_eof().unwrap();
+
+    while encoder.receive_packet(&mut encoded).is_ok() {
+        encoded.set_stream(video_out_index);
+
+        let out_stream = output_ctx.stream(video_out_index).unwrap();
+
+        encoded.rescale_ts(encoder.time_base(), out_stream.time_base());
+
+        encoded.set_position(-1);
+
+        encoded.write_interleaved(output_ctx).unwrap();
     }
     let err = output_ctx.write_trailer();
 
@@ -330,32 +538,35 @@ fn main() {
 
     println!("finished");
 
-    // unsafe {
-    //     println!("pb after trailer = {:?}", (*output_ctx.as_ptr()).pb);
-    // }
-    // unsafe {
-    //     println!(
-    //         "libavformat version: {}",
-    //         ffmpeg_next::ffi::avformat_version()
-    //     );
-    // }
+    unsafe {
+        println!("pb after trailer = {:?}", (*output_ctx.as_ptr()).pb);
+    }
+    unsafe {
+        println!(
+            "libavformat version: {}",
+            ffmpeg_next::ffi::avformat_version()
+        );
+    }
 
-    // unsafe {
-    //     let ctx = output_ctx.as_ptr();
+    unsafe {
+        let ctx = output_ctx.as_ptr();
 
-    //     println!("flags = {:x}", (*(*ctx).oformat).flags);
-    //     println!("url = {:?}", std::ffi::CStr::from_ptr((*ctx).url).to_str());
-    // }
-    // unsafe {
-    //     let ctx = output_ctx.as_mut_ptr();
+        println!("flags = {:x}", (*(*ctx).oformat).flags);
+        println!("url = {:?}", std::ffi::CStr::from_ptr((*ctx).url).to_str());
+    }
+    unsafe {
+        let ctx = output_ctx.as_mut_ptr();
 
-    //     if !ctx.is_null() && ((*(*ctx).oformat).flags & ffmpeg_next::ffi::AVFMT_NOFILE) == 0 {
-    //         if !(*ctx).pb.is_null() {
-    //             let ret = ffmpeg_next::ffi::avio_closep(&mut (*ctx).pb);
-    //             println!("avio_closep -> {}", ret);
-    //         }
-    //     }
+        if !ctx.is_null() && ((*(*ctx).oformat).flags & ffmpeg_next::ffi::AVFMT_NOFILE) == 0 {
+            if !(*ctx).pb.is_null() {
+                let ret = ffmpeg_next::ffi::avio_closep(&mut (*ctx).pb);
+                println!("avio_closep -> {}", ret);
+            }
+        }
 
-    //     ffmpeg_next::ffi::avformat_free_context(ctx);
-    // }
+        // ffmpeg_next::ffi::avformat_free_context(ctx);
+    }
+    drop(output);
+    #[cfg(windows)]
+    rename_mpd_tmp().unwrap()
 }
